b592007a37ff5e50c0db8d12de133c5fc1a0cc8a
Matt <carlotta.matt@gmail.com>
2019-03-22 15:26:06 -0700

    Add async from server example

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../images/logo.svg';
 import AvatarFromFiles from '../AvatarFromFiles';
 import AvatarFromServer from '../AvatarFromServer';
-import { app, logoContainer, logoStyle, title } from './styles.scss';
+import { app, loading, logoContainer, logoStyle, title } from './styles.scss';
 
-export default () => (
-  <div className={app}>
-    <div className={logoContainer}>
-      <img className={logoStyle} src={logo} alt="" />
-      <h1 className={title}>React Starter Kit</h1>
-      <h1 className={title}>Edit ./components and save to reload.</h1>
-      <AvatarFromFiles />
-      <AvatarFromServer head="head2" eyes="eyes2" mouth="mouth2" />
+const data = {
+  head: 'head2',
+  eyes: 'eyes2',
+  mouth: 'mouth2',
+};
+
+const fetchAvatarFromAPI = status =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (status === 200) {
+        resolve({ data });
+      } else {
+        reject('Unable to locate avatar!');
+      }
+    }, 1500);
+  });
+
+class Home extends Component {
+  state = { isLoading: true, head: '', eyes: '', mouth: '' };
+
+  componentDidMount = () => {
+    this.fetchAvatar();
+  };
+
+  fetchAvatar = () => {
+    fetchAvatarFromAPI(200)
+      .then(({ data }) => {
+        this.setState({ isLoading: false, ...data });
+      })
+      .catch(err => this.setState({ err: err.toString() }));
+  };
+
+  render = () => (
+    <div className={app}>
+      <div className={logoContainer}>
+        <img className={logoStyle} src={logo} alt="" />
+        <h1 className={title}>React Starter Kit</h1>
+        <h1 className={title}>Edit ./components and save to reload.</h1>
+        <AvatarFromFiles />
+        {this.state.isLoading ? (
+          <img className={loading} src={logo} alt="" width={100} />
+        ) : (
+          <AvatarFromServer
+            head={this.state.head}
+            eyes={this.state.eyes}
+            mouth={this.state.mouth}
+          />
+        )}
+      </div>
+      {this.state.err && <p>{this.state.err} </p>}
     </div>
-  </div>
-);
+  );
+}
+
+export default Home;
